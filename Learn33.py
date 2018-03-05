965c6aa552b52a2c205e4ac518fed5eb39b220ad
ArlenZhang <longyin128128@163.com>
2018-03-05 08:54:49 +0800

    add

--- Learn33.py
+++ Learn33.py
@@ -1,4 +1,10 @@
 from urllib.request import urlopen
-html = urlopen(
-    "www.baidu.com"
-).read().decode('utf-8')  # chinese, decode the text
+from bs4 import BeautifulSoup
+html = urlopen("http://www.cs.toronto.edu/~weifeng/software.html").read().decode("utf-8")
+print(html)
+
+# 定义soup来解析html代码
+soup = BeautifulSoup(html, features='lxml')  # 有几种解析方式，这里可以选lxml解析方法
+
+# 用对象和属性的方式获取html中的元素
+# 例如:
